b8646e05a183ef9954f6f5dcc6a55fb3176889b3
VirajB7 <busaviraj743@gmail.com>
2024-12-19 18:57:17 +0530

    feat: solve mismatch type error

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req: Request, res: Response) => {
-  return res.send("It's working 🙌");
+   res.send("It's working 🙌");
 });
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
